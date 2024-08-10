2636787ffe10bee0ee053526c258bf307e3ba1ee
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-10 16:25:58 -0500

    Initial project setup

---
+++ src/main/java/com/mrvl/loginsystemtemplate/LoginSystemTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.mrvl.loginsystemtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSystemTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSystemTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrvl/loginsystemtemplate/LoginSystemTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrvl.loginsystemtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSystemTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
