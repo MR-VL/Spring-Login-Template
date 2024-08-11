b923c59ec475fb153ed6fd640334abdfd6871b50
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-11 16:27:36 -0500

    Implement user and Role

--- Backend/src/main/java/com/mrvl/loginsystemtemplate/user/User.java
+++ Backend/src/main/java/com/mrvl/loginsystemtemplate/user/User.java
@@ -1,5 +1,6 @@
 package com.mrvl.loginsystemtemplate.user;
 
+import com.mrvl.loginsystemtemplate.role.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,11 +11,15 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.security.Principal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import static jakarta.persistence.FetchType.EAGER;
 
 @Getter
 @Setter
@@ -41,6 +46,8 @@ public class User implements UserDetails, Principal {
     private boolean accountLocked;
     private boolean enabled;
 
+    @ManyToMany(fetch = EAGER)
+    private List<Role> roles;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
@@ -52,7 +59,10 @@ public class User implements UserDetails, Principal {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return this.roles
+                .stream()
+                .map(r -> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
