bb9e2550c4ed3787cf4e13c9ba803a03bf49c15c
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-13 16:23:12 -0500

    Finish JWTSERVICE

--- Backend/src/main/java/com/mrvl/loginsystemtemplate/security/JwtService.java
+++ Backend/src/main/java/com/mrvl/loginsystemtemplate/security/JwtService.java
@@ -59,9 +59,25 @@ public class JwtService {
         byte[] key = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(key);
     }
-    public String extractUsername(String jwt) {
-        return null;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
@@ -69,5 +85,10 @@ public class JwtService {
     }
 
     private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
